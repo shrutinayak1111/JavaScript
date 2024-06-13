142fceaba1841d76994cea6843162aa6b481bc61
shrutinayak1111 <162023055+shrutinayak1111@users.noreply.github.com>
2024-06-13 16:04:53 +0000

    new files added

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,5 @@
+const name="shruti"
+let id=214101
+var city='bbsr'
+school="prakash"
+console.table([name,id,city,school])
---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,11 @@
+// Number
+// String
+// BigInt
+// Boolean
+// null=standalone value
+// undefined=value is not assigned
+// let state;
+// Symbol
+console.log(typeof "shruti");
+console.log(typeof null);
+console.log(typeof undefined);
