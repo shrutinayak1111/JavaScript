b893874f9e5955fec6680c69383b59b810454a6c
shrutinayak1111 <162023055+shrutinayak1111@users.noreply.github.com>
2024-06-13 17:08:20 +0000

    conversion

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,50 @@
+// let score="abv33";
+// let score1="33"
+// new_score1=Number(score)
+// new_score=Number(score1)
+// console.table ([ typeof score,typeof score1,typeof new_score1]);
+
+// isValid=0
+// bool=Boolean(isValid)
+// console.log(bool)
+
+// 0=>false
+// " "=>false
+// 1=>TRUE
+// "SHRUTI"=>true
+
+
+// let name="shruti"
+// console.log(Number(name));
+// NaN or not applicable
+
+
+// let num=33
+// console.log(String(num));
+// console.log(typeof(num));
+//possible
+
+
+
+//*****************OPERATION *********************/
+// console.table([2+2 , 2*3 ,4-2,2%4,4/2,2**2]);
+// console.table(["shruti"+" nayak"]);
+// no subtraction ,division is allowed
+
+// console.table(["shruti"*" nayak"]);
+// NaN
+
+// console.table(["shruti"*2]);
+// NaN
+
+// console.table(["2"+"3"]);
+// '2 3'
+
+// console.table(["2"+3]);
+// 23
+
+// console.table(["2"+3+3])
+// 233
+
+// console.table([2+3+"2"])
+// 52
