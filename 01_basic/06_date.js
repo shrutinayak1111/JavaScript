a8efcae6cfb3a9ff1b07c357ec1d42ae40d32f82
shrutinayak1111 <162023055+shrutinayak1111@users.noreply.github.com>
2024-06-14 08:04:59 +0000

    date

---
+++ 01_basic/06_date.js
@@ -0,0 +1,16 @@
+// let myDate=new Date()
+
+// // console.log(myDate);
+// // console.log(myDate.toString()); -->Fri Jun 14 2024 06:58:58 GMT+0000 (Coordinated Universal Time)
+// // console.log(myDate.toDateString()); -->Fri Jun 14 2024
+// // console.log(typeof myDate); -->object
+
+// let date=new Date(2013,0,3)
+// console.log((date.toDateString()));
+// console.log((new Date.toDateString(2014,6,3)));
+
+
+let timeStamp=Date.now()
+//  console.log(timeStamp); -->1718349228882
+//  console.log(timeStamp.getTime());
+ 
