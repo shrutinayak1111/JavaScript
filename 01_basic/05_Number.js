4e2aed2f4025ba6c9104dd6d630da8ca6a8f2506
shrutinayak1111 <162023055+shrutinayak1111@users.noreply.github.com>
2024-06-14 06:29:45 +0000

    Number_and_Math

---
+++ 01_basic/05_Number.js
@@ -0,0 +1,27 @@
+
+//*******************NUMBERS************* */
+// const score=333
+// console.log(score);
+// const newScore=new Number(33)
+// console.log(newScore);
+// console.log(newScore.toString().length);
+// console.log(newScore.toFixed(3));
+// const n=10000000000000000000000;
+// console.log(n.toLocaleString('en-IN'))
+
+
+
+
+//*************************MATHS******************** */
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.3));
+// console.log(Math.ceil(4.6));
+// console.log(Math.floor(4.3));
+// console.log(Math.floor(4.6));
+// console.log(Math.sqrt(4));
+console.log(Math.random()*10);
+
+const min=10
+const max=20
+console.log(Math.floor(Math.random()*(max-min+1))+min);
