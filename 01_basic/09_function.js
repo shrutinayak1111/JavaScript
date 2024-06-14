9a03b7f945f233c1103f3f2d435ce5b3be3a82ac
shrutinayak1111 <162023055+shrutinayak1111@users.noreply.github.com>
2024-06-14 17:45:55 +0000

    function

---
+++ 01_basic/09_function.js
@@ -0,0 +1,42 @@
+// // function sum(a,b){
+// //     return a+b;
+// // }
+// // const res=sum(2,3);
+// // console.log(res)
+
+
+// //if values are not passed we get output as undefined
+
+// function sum1(...a){
+//     return a;
+// }
+// const res1=sum1(2,4,5,6,7,8,3);
+// console.log(res1)
+
+
+
+// const user={
+//     username:"shruti",
+//  price:299
+// }
+
+// function handleObject(any){
+//     console.log(`name is ${any.username} and price is ${any.price}`);
+
+// }
+// console.log(handleObject(user));
+//  or
+// handleObject({
+//     username:"shruti",
+//  price:299
+// })
+
+
+const arr=[100,2400,477]
+ function newArr(getArray){
+    return getArray[1]
+ }
+//  console.log(newArr(arr));
+
+console.log(newArr([100,2400,477]));
+    
