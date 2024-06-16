558ee2c7baf9125e74bc37c184268528e4c8c9e6
shrutinayak1111 <162023055+shrutinayak1111@users.noreply.github.com>
2024-06-16 09:27:17 +0000

    map_filter_reduce

---
+++ 01_basic/14_map_reduce_filter.js
@@ -0,0 +1,64 @@
+// const alpha=['a','b','c','d']
+// const ans=alpha.forEach((items)=>{ 
+//     console.log(items);-->['a','b','c','d']
+// })
+// console.log(ans);-->undefined
+// forEach do not return any value
+
+
+//FILTER
+// const num=[1,2,3,4,5,6,7,8,9] 
+// const a=num.filter((n)=>n>4)
+// console.log(a);
+
+
+// const newn=[]
+// num.forEach((num)=>{
+//     if(num>4){
+//         newn.push(num)
+//     }
+//     }
+// )
+// console.log(newn);
+
+
+
+
+//MAP
+// const num=[1,2,3,4,5,6,7,8,9] 
+// const n=num.map((item)=>item+10) 
+// console.log(n);
+
+
+// newn=[]
+//     num.forEach((item)=>{
+//         newn.push(item+10)
+//     })
+// console.log(newn);
+
+
+
+// const num=[1,2,3,4,5,6,7,8,9] 
+//  const ans=num.map((n)=>n*10)
+//  .map((n)=>n+10)
+//  .filter((n)=>n>60)
+//  console.log(ans);
+
+
+
+//REDUCE
+
+const num=[1,2,3,4,5,6,7,8,9]
+// const n=num.reduce(function(acc,currval){
+//     return acc+currval
+// },0)
+// console.log(n);
+
+
+// const n=num.reduce((acc,curr)=>{
+//     return acc+curr
+// },0)
+// console.log(n);
+
+// const n=num.reduce((acc,curr)=> acc+curr,0)
+// console.log(n);
