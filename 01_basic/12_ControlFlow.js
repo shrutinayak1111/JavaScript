ec77f2880f26c40f7ba31e3a0e6e53225a916a8e
shrutinayak1111 <162023055+shrutinayak1111@users.noreply.github.com>
2024-06-16 06:14:44 +0000

    controlflow

---
+++ 01_basic/12_ControlFlow.js
@@ -0,0 +1,76 @@
+// const day=3;
+// switch(day){
+//     case 1:
+//         console.log("mon");
+//         break;
+//         case 2:
+//             console.log("tues");
+//             break;
+//             case 3:
+//                 console.log("wed");
+//                 break;
+//                 case 4:
+//                     console.log("thursday");
+//                     break;
+//                     case 5:
+//                         console.log("fri");
+//                         break;
+//                         case 6:
+//                             console.log("sat");
+//                             break;
+//                             case 7:
+//                                 console.log("sun");
+//                                 break;
+//                                 default:
+//                                     break;
+// }
+
+
+
+
+
+// falsy value 
+// false
+// 0
+// -0
+// BigInt 0n
+// ""
+// null
+// undefined
+// Nan
+
+
+
+// truthy value
+// "0"
+// 'false'
+// ' '
+// {}
+// []
+// function(){}
+
+
+
+
+
+
+// Nullish Coalescing Operator (??) : null undefined 
+// val =5?? 10
+// val1 =null?? 10
+// val2 =undefined?? 10
+// val3 =undefined?? 10??15
+
+
+
+// console.log(val);
+// console.log(val1);
+// console.log(val2);
+// console.log(val3);
+
+
+
+// **********************************TERNIARY OPERATOR**************************************
+// condition?true:false 
+let a=4;
+let b=5;
+console.log((a>b)?a:b);
